b6bc16ddc8db354d09b8da2d7b49a496192048cb
NekoRectifier <hyx123250@gmail.com>
2022-08-19 11:46:02 +0800

    Add Feature: Auto detect json file status Add Feature: Able to convert jpg to png

--- gen_data.py
+++ gen_data.py
@@ -1,8 +1,10 @@
 import json
 import os
+import shutil
+
+from PIL import Image
 from cityscapesscripts.preparation.json2instanceImg import json2instanceImg
 from cityscapesscripts.preparation.json2labelImg import json2labelImg
-import shutil
 from loguru import logger
 
 # ================ CONFIG ================ #
@@ -17,6 +19,7 @@ val_file_num = 0
 gtFine_paths = ["gtFine/train/aachen/", "gtFine/val/frankfurt/"]
 r_path_lI8b_train = "leftImg8bit/train/aachen/"
 r_path_lI8b_val = "leftImg8bit/val/frankfurt/"
+
 
 # ================ CONFIG ================ #
 
@@ -34,6 +37,48 @@ def _gen_pic(type: int, i):
         real_name,
         gtFine_paths[type] + city_names[type] + seq_num + '_000019_gtFine_instanceIds.png',
         'ids')
+
+
+def _cvt_jpg2png(path, num):
+    for i in range(num):
+        real_name = path + str(i + 1)
+        logger.info('Processing ' + real_name)
+        img = Image.open(real_name + '.jpg')
+        img.save(real_name + '.png')
+        os.remove(real_name + '.jpg')
+
+
+def _json_process(path, num):
+    for i in range(num):
+        real_name = path + str(i + 1) + '.json'
+        logger.info('Currently handling:' + real_name)
+
+        with open(real_name, "r", encoding="utf-8") as data_f:
+            json_data = json.load(data_f)
+            try:
+                # 1st of del
+                json_data.pop('imageData')
+                json_data.pop('flags')
+                json_data.pop('imagePath')
+                json_data.pop('version')
+
+                json_data['imgWidth'] = json_data.pop('imageWidth')
+                json_data['imgHeight'] = json_data.pop('imageHeight')
+                json_data['objects'] = json_data.pop('shapes')
+                objs = json_data['objects']
+                for obj in objs:
+                    obj.pop('flags')
+                    obj.pop('group_id')
+                    obj.pop('shape_type')
+
+                    obj['polygon'] = obj.pop('points')
+            except KeyError:
+                logger.error('Some keys in the json file has been modified, formatting operation stopped.')
+                return KeyError
+
+        with open(real_name, 'w', newline='\n') as data_f:
+            da = json.dumps(json_data, indent=4, sort_keys=True)
+            data_f.write(da)
 
 
 def generate_img():
@@ -55,6 +100,12 @@ def batch_rename():
     global train_file_num, val_file_num
     try:
         logger.info('Copying and Renaming train files...')
+
+        if (not os.path.exists("raw_train/1.png")) and os.path.exists("raw_train/1.jpg"):  # TODO more precise
+            logger.warning('Jpg image file detected, using Pillow to reformat')
+            _cvt_jpg2png('raw_train/', train_file_num)
+            _cvt_jpg2png('raw_val/', val_file_num)
+
         for i in range(train_file_num):
             seq_num = str(i).rjust(6, '0')
             real_name = 'raw_train/' + str(i + 1)
@@ -73,43 +124,24 @@ def batch_rename():
                             r_path_lI8b_val + 'frankfurt_' + seq_num + '_000019_leftImg8bit.png')
     except FileNotFoundError as e:
         if e.errno == 2:
-            logger.error("Original picture in png format is not found. This generator is only supported with .png files")
+            logger.error(
+                "File not found")
 
 
 def format_json():
-
     # dedicated to json files created by Labelme Application
+    logger.info('Formatting json files...')
+    skip = False
 
-    files = os.listdir()
-    for file in files:
-        if file.endswith('.json'):
-            logger.info('Currently handling:' + file)
-            with open(file, "r", encoding="utf-8") as data_f:
-                json_data = json.load(data_f)
+    with open('raw_train/1.json', "r", encoding="utf-8") as data_f:
+        json_data = json.load(data_f)
+        if "version" not in json_data and "imageData" not in json_data:
+            logger.warning('Json files has been formatted before, skipping this process')
+            skip = True
 
-                try:
-                    # 1st of del
-                    json_data.pop('imageData')
-                    json_data.pop('flags')
-                    json_data.pop('imagePath')
-                    json_data.pop('version')
-
-                    json_data['imgWidth'] = json_data.pop('imageWidth')
-                    json_data['imgHeight'] = json_data.pop('imageHeight')
-                    json_data['objects'] = json_data.pop('shapes')
-                    objs = json_data['objects']
-                    for obj in objs:
-                        obj.pop('flags')
-                        obj.pop('group_id')
-                        obj.pop('shape_type')
-
-                        obj['polygon'] = obj.pop('points')
-                except KeyError:
-                    logger.error('Some keys in the json file has been modified, formatting operation stopped.')
-
-            with open(file, 'w', newline='\n') as data_f:
-                da = json.dumps(json_data, indent=4, sort_keys=True)
-                data_f.write(da)
+    if not skip:
+        _json_process('raw_train/', train_file_num)
+        _json_process('raw_val/', val_file_num)
 
 
 def access_file():
@@ -129,7 +161,6 @@ def access_file():
         if not os.path.exists(path):
             logger.warning(r'{} folder is missing'.format(path))
             check = False
-            break
 
     if not check:
         logger.info('Reconstructing folders now...')
@@ -144,7 +175,7 @@ def access_file():
 
 
 if __name__ == "__main__":
-    format_json()
     access_file()
+    format_json()
     generate_img()
     batch_rename()
